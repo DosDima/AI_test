f2d3602a6547f52ccd1aebea6ba9faf9b8439b5c
DosDima <dosdima89@gmail.com>
2024-10-21 14:17:57 +0300

    add css styles and project structure

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Page content</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  title: "Home page",
+});
+
+const route = useRoute();
+
+console.log(route.meta.title); // My home page
+</script>
